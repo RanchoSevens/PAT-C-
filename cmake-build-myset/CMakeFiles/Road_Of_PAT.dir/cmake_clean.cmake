a9f93860c21c52494732ea1ec83014c2e6826e00
rancho <dyzhuo@zju.edu.cn>
2019-04-24 19:37:41 +0800

    你的注释

---
+++ cmake-build-myset/CMakeFiles/Road_Of_PAT.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Road_Of_PAT.dir/b1030.cpp.o"
+  "Road_Of_PAT.pdb"
+  "Road_Of_PAT"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Road_Of_PAT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
